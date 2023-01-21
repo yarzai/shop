80a9660dbdeb364536450277c7c4c73b4963d1d3
Aziz Rahman Yarzai <azizulrahman.yarzai@gmail.com>
2023-01-21 16:53:36 +0430

    projec initialized

---
+++ products/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+
+@login_required
+def welcome(request):
+
+    # return HttpResponse("<h1>Hi</h1>")
+    val = "Hi"
+    print(dir(request))
+    print(request.user)
+
+    return render(request, 'products/welcome.html', {"result": val, "test": 12})
+
+
+def test():
+    pass
