236c1fdc5e903a88e0ec177af70c55e0353a44fd
Aziz Rahman Yarzai <azizulrahman.yarzai@gmail.com>
2023-01-23 17:33:59 +0430

    Jan 23

--- products/models.py
+++ products/models.py
@@ -4,12 +4,22 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150, null=True, blank=True)
     price = models.IntegerField()
+    # binray = models.BinaryField(max_length=20, null=True, editable=True)
+    is_avaliable = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        db_table = "product"
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "category"
